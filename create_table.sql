2929d120686e0709aae7d9b67594c6f429bd37ba
jiroken25 <k.hirota0205@gmail.com>
2020-08-29 12:05:15 +0800

    updated as feedback

--- create_table.sql
+++ create_table.sql
@@ -1,29 +1,29 @@
-create table match_table(
 
-id int primary key,
-date date,
-location varchar,
-weight_class varchar,
-winner varchar,
-loser varchar
-
+CREATE TABLE "match" (
+    "id" int   NOT NULL,
+    "date" date   NOT NULL,
+    "location" varchar   NOT NULL,
+    "weight_class" varchar   NOT NULL,
+    "winner_fighter_id" int   NOT NULL,
+    "loser_fighter_id" int   NOT NULL,
+    CONSTRAINT "pk_match" PRIMARY KEY (
+        "id"
+     )
 );
 
-create table match_new_table(
-
-id int primary key,
-date date,
-location varchar,
-weight_class varchar,
-winner varchar,
-loser varchar
-
+CREATE TABLE "fighter" (
+    "id" int   NOT NULL,
+    "fighter_name" varchar   NOT NULL,
+    "height" float   NOT NULL,
+    "weight" float   NOT NULL,
+    CONSTRAINT "pk_fighter" PRIMARY KEY (
+        "id"
+     )
 );
 
+ALTER TABLE "match" ADD CONSTRAINT "fk_match_winner_fighter_id" FOREIGN KEY("winner_fighter_id")
+REFERENCES "fighter" ("id");
 
-create table fighter_table(
-id int primary key,
-fighter_name varchar,
-height numeric,
-weight numeric
-);
+ALTER TABLE "match" ADD CONSTRAINT "fk_match_loser_fighter_id" FOREIGN KEY("loser_fighter_id")
+REFERENCES "fighter" ("id");
+
